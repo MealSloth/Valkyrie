2ccb244d10ad29635c85cde03da80697f21e3488
Michael <michael@mealsloth.com>
2016-02-04 00:53:08 -0500

    VALKYRIE-10 VALKYRIE-17

--- Valkyrie/templatetags/tags_users.py
+++ Valkyrie/templatetags/tags_users.py
@@ -9,3 +9,12 @@ register = template.Library()
 def entry_for_user_gender(dictionary):
     return Gender.Gender[int(dictionary.get("gender"))][1]
 
+
+@register.simple_tag
+def status_for_status(status):
+    return PostStatus.PostStatus[int(status)][1]
+
+
+@register.simple_tag
+def status_for_status_lower(status):
+    return str(PostStatus.PostStatus[int(status)][1]).lower()
--- Valkyrie/view/user/view_user.py
+++ Valkyrie/view/user/view_user.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.template import Context
 from django.shortcuts import render
-from _include.Chimera.models import User
+from _include.Chimera.models import User, Post
 
 
 def user(request, user_id):
     current_user = User.objects.filter(id=user_id).values()[0]
-    response = render(request, 'page/user/user.html', Context({"user": current_user}))
+    posts = Post.objects.filter(chef_id=current_user.get('chef_id')).values()
+    response = render(request, 'page/user/user.html', Context({'user': current_user, 'posts': posts}))
     return HttpResponse(response)
