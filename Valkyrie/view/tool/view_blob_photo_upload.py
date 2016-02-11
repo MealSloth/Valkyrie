b2a54d572d3e8c3b63d48afa1d41df1bc25ba36a
Michael <michael@mealsloth.com>
2016-02-11 17:54:55 -0500

    Preparing for content_type change

--- Valkyrie/view/tool/view_blob_photo_upload.py
+++ Valkyrie/view/tool/view_blob_photo_upload.py
@@ -1,5 +1,5 @@
 from _include.Hydra.Hydra.form.photo.form_blob_photo_upload import BlobPhotoUploadForm
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.shortcuts import render
 from django.template import Context
 import urllib2
@@ -9,12 +9,21 @@ import urllib
 def blob_photo_upload(request):
     if request.method == 'POST':
         form = BlobPhotoUploadForm(request.POST, request.FILES)
-        form.set_origin('Valkyrie')
+
         if form.is_valid():
-            response = urllib2.urlopen('http://blob.mealsloth.com/blob-photo-upload/', urllib.urlencode(form))
-            result = response.read()
-            print(result)
-            return HttpResponseRedirect('/tools')
+            try:
+                with open(form.file) as image:
+                    response = urllib.urlopen('http://api.mealsloth.com/blob-photo-upload/',
+                                              urllib.urlencode({'file': image, }))
+                    result = response.read()
+                    print(result)
+                    return HttpResponseRedirect('/tools')
+            except IOError:
+                return HttpResponseRedirect('/users')
+            # response = urllib2.urlopen('http://blob.mealsloth.com/blob-photo-upload/', urllib.urlencode(form))
+            # result = response.read()
+            # print(result)
+            # return HttpResponseRedirect('/tools')
         else:
             return HttpResponse("Invalid form")
     else:
