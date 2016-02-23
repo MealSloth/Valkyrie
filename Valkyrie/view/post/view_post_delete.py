cd78fe58a37764052a4157edefe57af9d28e5476
Michael <michael@mealsloth.com>
2016-02-23 04:36:05 -0500

    Adapting new /post/delete

--- Valkyrie/view/post/view_post_delete.py
+++ Valkyrie/view/post/view_post_delete.py
@@ -1,16 +1,12 @@
-from _include.Chimera.Chimera.settings import PROTOCOL
+from _include.Chimera.Chimera.view.post.view_post_delete import post_delete as chimera_post_delete
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from json import dumps
-import urllib2
 
 
 def post_delete(request, post_id):
+    post_delete_kwargs = {'post_id': post_id}
     try:
-        data = {'post_id': post_id}
-        data = dumps(data)
-        urllib2.urlopen(PROTOCOL + 'api.mealsloth.com/post/delete/', data)
-    except urllib2.HTTPError:
-        return HttpResponseRedirect(reverse('post', args=[post_id, ]))
-
+        chimera_post_delete(request=None, **post_delete_kwargs)
+    except StandardError:
+        HttpResponseRedirect(reverse('post', args=[post_id, ]))
     return HttpResponseRedirect('/posts')
