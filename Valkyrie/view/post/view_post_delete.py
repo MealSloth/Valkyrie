e22cf29bcd748d0ffcd6efd87508b1132abc079b
Michael <michael@mealsloth.com>
2016-02-23 04:32:58 -0500

    Adapting new /post/delete

--- Valkyrie/view/post/view_post_delete.py
+++ Valkyrie/view/post/view_post_delete.py
@@ -1,4 +1,3 @@
-from _include.Chimera.Chimera.models import Post, Order
 from _include.Chimera.Chimera.settings import PROTOCOL
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -7,31 +6,11 @@ import urllib2
 
 
 def post_delete(request, post_id):
-    post = Post.objects.filter(pk=post_id)
-    if post.count() > 0:
-        post = post[0]
-    else:
-        return HttpResponseRedirect('/posts')
-
-    orders_list = Order.objects.filter(post_id=post.id)
-    for order in orders_list:
-        try:
-            order.delete()
-        except StandardError:
-            return HttpResponseRedirect('/posts')
-
-    album_id = post.album_id
-
     try:
-        post.delete()
-    except StandardError:
-        return HttpResponseRedirect(reverse('post', args=[post.id, ]))
-
-    try:
-        data = {'album_id': album_id}
+        data = {'post_id': post_id}
         data = dumps(data)
-        urllib2.urlopen(PROTOCOL + 'api.mealsloth.com/album/delete/', data)
+        urllib2.urlopen(PROTOCOL + 'api.mealsloth.com/post/delete/', data)
     except urllib2.HTTPError:
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect(reverse('post', args=[post_id, ]))
 
     return HttpResponseRedirect('/posts')
