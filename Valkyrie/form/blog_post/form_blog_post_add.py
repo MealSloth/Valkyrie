5daa1a4a9bc5605ab1510037f2a99352a7e0ec91
Michael <michael@mealsloth.com>
2016-02-24 22:20:03 -0500

    Using internal blog post create

--- Valkyrie/form/blog_post/form_blog_post_add.py
+++ Valkyrie/form/blog_post/form_blog_post_add.py
@@ -1,9 +1,5 @@
-from _include.Chimera.Chimera.view.blob.view_blob_upload import blob_upload as upload_blob
-from _include.Chimera.Chimera.view.album.view_album_create import album_create as create_album
-from _include.Chimera.Chimera.storage_url_suffixes import StorageURLSuffixes
-from _include.Chimera.Chimera.models import BlogPost, Author
-from datetime import datetime
-from base64 import b64encode
+from _include.Chimera.Chimera.view.blog_post.view_blog_post_create import blog_post_create as create_blog_post
+from _include.Chimera.Chimera.models import Author
 from django.forms import *
 
 
@@ -22,32 +18,14 @@ class BlogPostAddForm(Form):
     image = FileField(required=True)
 
     def process(self):
-        author_id = self.cleaned_data['author_id']
-        title = self.cleaned_data['title']
-        short_description = self.cleaned_data['short_description']
-        long_description = self.cleaned_data['long_description']
-        image = self.cleaned_data['image']
-        post_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
-
-        album_create_kwargs = {'Please': 'Thank you', }
-
-        album = create_album(request=None, **album_create_kwargs)
-
-        blog_post = BlogPost(
-            album_id=album.id,
-            author_id=author_id,
-            title=title,
-            short_description=short_description,
-            long_description=long_description,
-            post_time=post_time,
-        )
-
-        blog_post.save()
-
-        blob_upload_kwargs = {
-            'file': b64encode(image.read()),
-            'album_id': str(blog_post.album_id),
-            'url_suffix': StorageURLSuffixes.get_url_suffix(StorageURLSuffixes.SIREN_BLOG),
+        blog_post_create_kwargs = {
+            'author_id': self.cleaned_data['author_id'],
+            'title': self.cleaned_data['title'],
+            'short_description': self.cleaned_data['short_description'],
+            'long_description': self.cleaned_data['long_description'],
+            'image': self.cleaned_data['image'],
         }
 
-        upload_blob(request=None, **blob_upload_kwargs)
+        blog_post = create_blog_post(request=None, **blog_post_create_kwargs)
+
+        return blog_post
--- Valkyrie/view/blog_post/view_blog_post_add.py
+++ Valkyrie/view/blog_post/view_blog_post_add.py
@@ -1,14 +1,13 @@
-from django.http import HttpResponseRedirect
-
 from Valkyrie.form.blog_post.form_blog_post_add import BlogPostAddForm
 from _include.Chimera.Chimera.settings import PROTOCOL
+from django.http import HttpResponseRedirect
 
 
 def blog_post_add(request):
     if request.method == 'POST':
         form = BlogPostAddForm(request.POST, request.FILES)
         if form.is_valid():
-            form.process()
-            return HttpResponseRedirect(PROTOCOL + 'mealsloth.com/blog/')
+            blog_post = form.process()
+            return HttpResponseRedirect(PROTOCOL + 'mealsloth.com/blog-post/%s' % str(blog_post.id))
 
     return HttpResponseRedirect('/blog-posts')
