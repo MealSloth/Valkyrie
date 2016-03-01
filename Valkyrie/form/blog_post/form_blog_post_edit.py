6bf9d70eb52cca4eea99ab99c693bbbf406a67d2
Michael <michael@mealsloth.com>
2016-03-01 12:22:55 -0500

    CHIMERA-35

--- Valkyrie/form/blog_post/form_blog_post_edit.py
+++ Valkyrie/form/blog_post/form_blog_post_edit.py
@@ -1,4 +1,5 @@
-from _include.Chimera.Chimera.models import BlogPost, Author
+from _include.Chimera.Chimera.view.blog_post.view_blog_post_edit import blog_post_edit as edit_blog_post
+from _include.Chimera.Chimera.models import Author
 from django.forms import *
 
 
@@ -16,21 +17,12 @@ class BlogPostEditForm(Form):
     long_description = CharField(max_length=10000, required=True, widget=Textarea)
 
     def process(self, blog_post_id):
-        author_id = self.cleaned_data['author_id']
-        title = self.cleaned_data['title']
-        short_description = self.cleaned_data['short_description']
-        long_description = self.cleaned_data['long_description']
+        blog_post_edit_kwargs = {
+            'blog_post_id': blog_post_id,
+            'author_id': self.cleaned_data['author_id'],
+            'title': self.cleaned_data['title'],
+            'short_description': self.cleaned_data['short_description'],
+            'long_description': self.cleaned_data['long_description'],
+        }
 
-        blog_post = BlogPost.objects.filter(pk=blog_post_id)
-
-        if blog_post.count() > 0:
-            blog_post = blog_post[0]
-        else:
-            return
-
-        blog_post.author_id = author_id
-        blog_post.title = title
-        blog_post.short_description = short_description
-        blog_post.long_description = long_description
-
-        blog_post.save()
+        edit_blog_post(request=None, **blog_post_edit_kwargs)
